6e406c023fef8cc01eb91156dac3e23b953d90ad
Lorenz Reweghs <lorenz.reweghs@student.ehb.be>
2021-05-18 14:49:18 +0200

    feat(javascript): added variables

---
+++ javascript/index.js
@@ -0,0 +1,7 @@
+let standardCost = 9.5;
+
+const drinks = cost => cost + 1.25;
+const food = cost => cost + 2.50;
+const threeD = cost => cost + 3.50;
+const marathon = cost => cost + 2.25;
+const vipSeats = cost => cost + 7;
