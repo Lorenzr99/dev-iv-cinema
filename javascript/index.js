4fff079ab9d2d0f7e0af1abbe9b3f5b28a37602f
Lorenz Reweghs <lorenz.reweghs@student.ehb.be>
2021-05-18 14:50:05 +0200

    feat(javascript): added pipeline

--- javascript/index.js
+++ javascript/index.js
@@ -5,3 +5,15 @@ const food = cost => cost + 2.50;
 const threeD = cost => cost + 3.50;
 const marathon = cost => cost + 2.25;
 const vipSeats = cost => cost + 7;
+
+const pipe = function (...fns) {
+    console.log(fns);
+    return function (price) {
+        return fns.reduce(function (acc, fn) {
+            return fn(acc);
+        }, price);
+    };
+};
+
+const decorator = pipe(drinks, food, threeD, marathon, vipSeats);
+console.log(decorator(standardCost));
