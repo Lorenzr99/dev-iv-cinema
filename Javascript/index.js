f6ea4c88dc540c0ab1d5e8cdd228e5536b906216
Fien Denblinden <fien.denblinden@student.ehb.be>
2021-05-23 11:05:32 +0200

    refactor: updated code

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,17 +1,25 @@
+// BRONNEN:
+
+// https://www.youtube.com/watch?v=P6XGSKO2RzI
+// https://stackabuse.com/quicksort-in-javascript/
+// https://www.guru99.com/quicksort-in-javascript.html
+
+// ////////////////////////////////////////////////////////////////////////////
+
 "use strict";
 
-let prices = [12.00, 10.75, 9.50, 11.75, 16.50, 13.00]; //Dit is een array van de prijzen van onze film tickets (met eventuele extra kosten bijgerekend)
+let prices = [12.00, 10.75, 9.50, 11.75, 16.50, 13.00]; // Dit is een array van de prijzen van onze film tickets (met eventuele extra kosten bijgerekend)
 
-function swap(prices, leftIndex, rightIndex) {
-    var temp = prices[leftIndex];
-    prices[leftIndex] = prices[rightIndex];
-    prices[rightIndex] = temp;
+function swap(prices, leftSide, rightSide) {
+    let temp = prices[leftSide];
+    prices[leftSide] = prices[rightSide];
+    prices[rightSide] = temp;
 }
 
 function divide(prices, left, right) {
-    let pivot = prices[Math.floor((right + left) / 2)], //middelste element bepalen (pivot)
-        i = left, //linkerkant van de pivot
-        j = right; //rechterkant van de pivot
+    let pivot = prices[Math.floor((right + left) / 2)], // Middelste element bepalen (pivot)
+        i = left, // Linkerkant van de pivot
+        j = right; // Rechterkant van de pivot
     while (i <= j) {
         while (prices[i] < pivot) {
             i++;
@@ -20,7 +28,7 @@ function divide(prices, left, right) {
             j--;
         }
         if (i <= j) {
-            swap(prices, i, j); //i en j wisselen (swappen)
+            swap(prices, i, j); // i en j wisselen 
             i++;
             j--;
         }
@@ -30,7 +38,7 @@ function divide(prices, left, right) {
 
 function sort(prices, left, right) {
     if (prices.length > 1) {
-        let i = divide(prices, left, right); //i returnen vanuit de functie "divide"
+        let i = divide(prices, left, right);
         if (left < i - 1) {
             sort(prices, left, i - 1);
         }
@@ -43,4 +51,4 @@ function sort(prices, left, right) {
 
 
 let sortedArray = sort(prices, 0, prices.length - 1);
-console.log(sortedArray, "Het Werkt"); //prints [9.5, 10.75, 11.75, 12, 13, 16.5]
+console.log(sortedArray, "Het Werkt"); // console log print: [9.5, 10.75, 11.75, 12, 13, 16.5] (van klein-groot)
